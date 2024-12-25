016902a63de213d961550c0d78978814de5123f1
sanjaiiam18 <sanjaim1772006@gmail.com>
2024-12-25 14:42:59 +0530

    final

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
 "use client";
 import Markdown from "markdown-to-jsx";
 import { useChat } from "ai/react";
+import { AiOutlineReload } from "react-icons/ai";
 import "./globals.css";
 import { IoMdSend } from "react-icons/io";
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="chat">
-      <h1 className="header">BOT🤖</h1>
+      <h1 className="header">
+        BOT🤖
+        <span className="reload" onClick={() => window.location.reload()}>
+          <AiOutlineReload />
+        </span>
+      </h1>
+
       <div className="chat-body">
         {messages.map((m) => (
           <div key={m.id} className={`message ${m.role}`}>
